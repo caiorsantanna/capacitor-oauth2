3d847e1f3b1c17f753232d41648e619d04512d1a
Caio Santanna <caiorsantanna@gmail.com>
2023-08-29 14:54:21 -0300

    feat: 🎸 dist

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,23 @@
+import { WebPlugin } from '@capacitor/core';
+import type { OAuth2AuthenticateOptions, OAuth2ClientPlugin, OAuth2RefreshTokenOptions } from "./definitions";
+export declare class OAuth2ClientPluginWeb extends WebPlugin implements OAuth2ClientPlugin {
+    private webOptions;
+    private windowHandle;
+    private intervalId;
+    private loopCount;
+    private intervalLength;
+    private windowClosedByPlugin;
+    /**
+     * Get a new access token using an existing refresh token.
+     */
+    refreshToken(_options: OAuth2RefreshTokenOptions): Promise<any>;
+    getAccessTokenFromCode(authorizationRedirectUrlParamObj: any, resolve: any, reject: any): void;
+    openWindow(resolve: any, reject: any): void;
+    authenticate(options: OAuth2AuthenticateOptions): Promise<unknown>;
+    private readonly MSG_RETURNED_TO_JS;
+    private requestResource;
+    assignResponses(resp: any, accessToken: string, authorizationResponse: any, accessTokenResponse?: any): void;
+    logout(options: OAuth2AuthenticateOptions): Promise<boolean>;
+    private closeWindow;
+    private doLog;
+}
